51fac08ef5d818027f98da4c4e5f5436587d344b
DixitTrivedi <44526020+DixitTrivedi@users.noreply.github.com>
2021-03-24 11:58:12 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,21 +1,15 @@
-# function for count sales of gloves and masks
-def count_sales(gloves, masks):
+def count_sales(masks, gloves):
     gloves = gloves
     mask = masks
 
-    unique_gloves = list(set(gloves))
-
-    # print(type(unique_gloves))
-    # print(type(unique_masks))
-
-    # print(unique_gloves)
-    # print(unique_masks)
+    unique_gloves = list(dict.fromkeys(gloves))
+    unique_masks = list(dict.fromkeys(mask))
+    #print(unique_gloves)
+    #print(unique_masks)
     count_list = []
     for i in  unique_gloves:
         counts = gloves.count(i)
         count_list.append(counts)
-
-
     pair = []
     for i in count_list:
         if i % 2 != 0:
@@ -23,20 +17,37 @@ def count_sales(gloves, masks):
             pair.append(i)
         else:
             pair.append(i)
-
-
     answer_list = []
+    sell = list(set(unique_masks) & set(pair))
     for i in pair:
         answer_list.append(i/2)
+    #print(sum(answer_list))
 
-    print(sum(answer_list))
+    #mask
+    mask_list = []
+    for m in unique_masks:
+        counts = mask.count(m)
+        mask_list.append(counts)
+    unique_gloves.sort()
+    unique_masks.sort()
 
-# load jason 
+    #print(answer_list)
+    #print(mask_list)
+    l = len(answer_list)
+    sell_list = []
+    for i in range(0,l):
+        final_sell = min(answer_list[i],mask_list[i])
+        sell_list.append(final_sell)
+    #print(sell_list)
+    print(sum(sell_list))
+    f.close()
+
+
 import json
-f = open('jsonData3.json')
-data = json.load(f)
+# f = open('jsonData.json',) 
+f = open('jsonData3.json',)
+data = json.load(f) 
 
-# function call
-count_sales(data['gloves'], data['masks'])
-
-f.close()
+count_sales(data['masks'], data['gloves'])
+  
+f.close() 
